45ec0afb1797a8b7178b098a932d582976a92e99
Brynne Fitzgerald <brynnefitzgerald@gmail.com>
2021-11-15 13:51:55 -0500

    Update model.py

---
+++ model.py
@@ -0,0 +1,23 @@
+from data_transformation import generate_table_final
+
+from statsmodels.formula.api import ols
+import statsmodels.api as sm
+from sklearn.linear_model import LinearRegression
+
+
+table_final = generate_table_final()
+#Train/test split
+table_final_TRAIN = table_final[:245]
+table_raw_TEST = table_raw[-6:]
+
+def model(table_final_TRAIN):
+    """
+    Function to create the model
+    args: table_final_TRAIN (pd.DataFrame)
+    returns: model_4 (object)
+    """
+    formula = 'quits ~  women_ratio + lfp_25_54 + job_openings_inversesqrt'
+    model4 = ols(formula = formula, data = table_final_TRAIN)
+    model_4 = model4.fit(cov_type = 'HAC', cov_kwds = {'maxlags':None}, use_t=True)
+
+    return model_4
