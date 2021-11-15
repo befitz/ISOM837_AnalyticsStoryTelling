4f1376ef14a48c40c39b850eeec5d69b17d240b2
Brynne Fitzgerald <brynnefitzgerald@gmail.com>
2021-11-15 13:56:34 -0500

    Update data_transformation.py

--- data_transformation.py
+++ data_transformation.py
@@ -1,4 +1,6 @@
 from fred_data_extract import build_seperations_table
+import pandas as pd
+import numpy as np
 
 variables = ['CES0000000039', 'LCEAPR01USM189S', 'LNS12027714',
              'TCU', 'USEPUINDXM', 'AWHNONAG', 'LNU01300012','LNS11300060','LNS11324230','JTS1000QUR', 'CPIAUCSL', 'JTSJOR']
